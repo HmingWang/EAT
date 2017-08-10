8e62d147b44b715789d372ebc71fa0ad3442537f
Xest <whaim1986@gmail.com>
2017-08-10 16:36:31 +0800

    init

--- EAT/Form1.Designer.cs
+++ EAT/Form1.Designer.cs
@@ -28,12 +28,144 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 444);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(605, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(83, 21);
+            this.toolStripStatusLabel1.Text = "Excel-A-Text";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(117, 21);
+            this.toolStripStatusLabel2.Text = "版本号：0.1 内测版";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(132, 21);
+            this.toolStripStatusLabel3.Text = "中山大学肖鹏博士团队";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开Excel文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "打开Text文件";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "开始转换";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 189);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "帮助说明";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(118, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(475, 429);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(605, 470);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Excel-A-Text";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- EAT/Form1.cs
+++ EAT/Form1.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using System.Reflection;
+
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
 
 namespace EAT
 {
@@ -15,6 +19,119 @@ namespace EAT
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+          
+            open.Filter= "Excel 工作簿|*.xlsx|Excel 97-2003 工作簿|*.xls";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = open.FileName;
+
+                //解析excel
+                LoadExcel(filePath);
+            }
+            
+        }
+
+        private void LoadExcel(string filePath)
+        {
+            Excel.Application app = new Excel.Application();
+            Excel.Sheets sheets;
+            Excel.Workbook workbook = null;
+            object oMissiong = Missing.Value;
+            DataTable dt = new DataTable();
+
+            workbook = app.Workbooks.Open(filePath, oMissiong, true, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong, oMissiong);
+            //将数据读入到DataTable中——Start    
+
+            sheets = workbook.Worksheets;
+            Excel.Worksheet worksheet = (Excel.Worksheet)sheets.get_Item(1);//读取第一张表  
+            if (worksheet == null)
+            {
+                MessageBox.Show("Excel表中没有数据");
+                return;
+            }
+
+
+            string cellContent;
+            string cellName;
+            int iRowCount = worksheet.UsedRange.Rows.Count;
+            int iColCount = worksheet.UsedRange.Columns.Count;
+            Excel.Range range;
+
+       
+            this.dataGridView1.DataSource = dt; 
+            dt.Columns.Add(new DataColumn("文件名"));
+            dt.Columns.Add(new DataColumn("内容"));
+            this.dataGridView1.Columns[1].Width = 300;
+
+
+            for (int i = 1; i <= iRowCount; ++i)
+            {
+                range = (Excel.Range)worksheet.Cells[i, 1];
+                cellName = range.Text.ToString();
+
+                range = (Excel.Range)worksheet.Cells[i, 2];
+                cellContent = range.Text.ToString();
+
+                DataRow dr = dt.NewRow();
+                dr[0] = cellName;
+                dr[1] = cellContent;
+
+                dt.Rows.Add(dr);
+            }
+
+
+            //将数据读入到DataTable中——End  
+            workbook.Close(false, oMissiong, oMissiong);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            app.Workbooks.Close();
+            app.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(app);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("版权说明: \n\n本软件由中山大学肖鹏博士团队开发\n设计策划：肖鹏，联系方式：cifangyue@163.com\n技术负责：王海明 肖鹏");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                string dirPath = folder.SelectedPath;
+
+                SaveFiles(dirPath);
+            }
+        }
+
+        private void SaveFiles(string dirPath)
+        {
+            DataTable dt = (DataTable)this.dataGridView1.DataSource;
+            foreach(DataRow dr in dt.Rows)
+            {
+                string filepath = dirPath + '/' + dr[0]+".txt";
+                if (File.Exists(filepath))
+                {
+                    MessageBox.Show("文件已存在:" + filepath);
+                    return;
+                }
+                FileStream fs = File.Create(filepath);
+                
+                byte[] content= System.Text.Encoding.Default.GetBytes(dr[1].ToString());
+                fs.Write(content, 0, content.Length);
+
+                fs.Close();
+            }
+
+            MessageBox.Show("转换完成");
         }
     }
 }
